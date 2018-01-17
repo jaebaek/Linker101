ff8c9a604d7849a50795ea22c1af7c9590bea3bb
Jaebaek Seo <jaebaek@chromium.org>
2018-01-17 18:31:01 +0900

    [wip] tinylinker

--- tinylinker/tinylinker.c
+++ tinylinker/tinylinker.c
@@ -33,7 +33,8 @@
 
 #include <stdio.h>
 
-#define dlog printf
+#define dlog(msg, ...) printf(msg "\n", __VA_ARGS__)
+#define dlog_i
 
 #include "tinylinker.h"
 #include "tinylinker_internal.h"
@@ -98,19 +99,10 @@ bool validate_ehdr(Elf64_Ehdr *ehdr, tltype_t type)
 
 void *load(Elf64_Ehdr *ehdr, Elf64_Phdr *phdr)
 {
-    /*
-    size_t max_align = 0;
-    for (unsigned i = 0;i < ehdr->e_phnum;++i) {
-        if (phdr[i].p_type == PT_LOAD && max_align < phdr[i].p_align)
-            max_align = phdr[i].p_align;
-    }
-    encl_base = roundup(max_align, encl_base);
-    */
-
     unsigned i;
-    uint64_t program = 0;
+    uint64_t vaddr_base = 0;
     uint64_t load_end = 0;
-    uint64_t mem = (uint64_t) ehdr;
+    uint64_t file = (uint64_t) ehdr;
 
     for (i = 0;i < ehdr->e_phnum;++i) {
         if (phdr[i].p_type == PT_LOAD && load_end < phdr[i].p_vaddr + phdr[i].p_memsz)
@@ -118,7 +110,7 @@ void *load(Elf64_Ehdr *ehdr, Elf64_Phdr *phdr)
     }
 
     if (load_end) {
-        program = (uint64_t) mmap(NULL, get_pages(load_end),
+        vaddr_base = (uint64_t) mmap(NULL, get_pages(load_end),
                 PROT_READ | PROT_WRITE | PROT_EXEC,
                 MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     } else
@@ -126,31 +118,181 @@ void *load(Elf64_Ehdr *ehdr, Elf64_Phdr *phdr)
 
     for (i = 0;i < ehdr->e_phnum;++i) {
         if (phdr[i].p_type == PT_LOAD) {
-            memcpy((void *)(program + phdr[i].p_vaddr),
-                   (void *)(mem + phdr[i].p_offset),
+            memcpy((void *)(vaddr_base + phdr[i].p_vaddr),
+                   (void *)(file + phdr[i].p_offset),
                     phdr[i].p_filesz);
             if (phdr[i].p_filesz < phdr[i].p_memsz)
-                bzero((void *)(program + phdr[i].p_vaddr + phdr[i].p_filesz),
+                bzero((void *)(vaddr_base + phdr[i].p_vaddr + phdr[i].p_filesz),
                         phdr[i].p_memsz - phdr[i].p_filesz);
 
             /* TODO: permission setup
                mprotect(((phdr[i].p_flags & PF_R) ? PROT_EXEC : 0) |
                ((phdr[i].p_flags & PF_W) ? PROT_WRITE : 0) |
                ((phdr[i].p_flags & PF_X) ? PROT_READ : 0))
-        } else if (phdr[i].p_type == PT_DYNAMIC) {
-            dyn = (Elf64_Dyn*)( encl_base + phdr[i].p_vaddr );
-        }
                */
         }
     }
-    return (void *) program;
+    return (void *) vaddr_base;
+}
+
+uint64_t find_dynamic(Elf64_Ehdr *ehdr, Elf64_Phdr *phdr)
+{
+    unsigned i;
+    for (i = 0;i < ehdr->e_phnum;++i) {
+        if (phdr[i].p_type == PT_DYNAMIC)
+            return phdr[i].p_vaddr;
+    }
+    return 0;
+}
+
+uint32_t update_dynsym(Elf64_Shdr *shdr, uint32_t shnum, Elf64_Sym *sym, uint64_t symtab)
+{
+    unsigned i;
+    uint32_t symtabsz = 0;
+    uint64_t sym_vaddr;
+
+    /* get symbol table size */
+    for (i = 0; i < shnum; ++i) {
+        if (shdr[i].sh_addr == symtab) {
+            symtabsz = shdr[i].sh_size;
+            break;
+        }
+    }
+    if (symtabsz == 0) {
+        dlog_i("%u: .dynsym has 0 size", __LINE__);
+        return 0;
+    }
+
+    /* iterate .dynsym */
+    for (i = 1; i < symtabsz/sizeof(Elf64_Sym); ++i) {
+        if (sym[i].st_shndx == SHN_UNDEF) {
+            sym_vaddr = 0;
+            /* get symbol from pre-define set */
+            /* get symbol from another shared lib */
+                // dlog("%u th symbol in .dynsym is not found", i);
+        }
+    }
+}
+
+//void Loader::get_symbols(extsym_t *esym, size_t nesym)
+//{
+//    Elf64_Sym *sym = (Elf64_Sym *)(encl_base + dinfo.symtab);
+//    char *strtab = (char *)(encl_base + dinfo.strtab);
+//    for (unsigned i = 1; i < dinfo.symtabsz/sizeof(Elf64_Sym); ++i) {
+//        if (sym[i].st_shndx != SHN_UNDEF) {
+//            for (unsigned j = 0;j < nesym;++j)
+//                if (!strcmp(esym[j].name, &strtab[sym[i].st_name])) {
+//                    *(esym[j].value) = sym[i].st_value;
+//                    dlog("%s is %lx\n", esym[j].name, *(esym[j].value));
+//                }
+//        }
+//    }
+//}
+
+#define GET_DT(elem) \
+    dinfo->elem = (uint64_t)e->d_un.d_ptr; break
+bool read_dynamic(Elf64_Dyn *dyn, dyninfo_t *dinfo)
+{
+    Elf64_Dyn *e;
+
+    /* read entries of dynamic section */
+    for (e = dyn;e->d_tag != DT_NULL;++e) {
+        switch (e->d_tag) {
+            case DT_NEEDED:
+                dlog("%u: DT_NEEDED (dynamic linking) is not supported", __LINE__);
+                return false;
+            case DT_HASH: GET_DT(hash);
+
+            //---- symbol and relocation tables related ----
+            case DT_STRTAB: GET_DT(strtab);
+            case DT_SYMTAB: GET_DT(symtab);
+            case DT_STRSZ: GET_DT(strsz);
+            case DT_SYMENT:
+                if (e->d_un.d_val != sizeof(Elf64_Sym)) {
+                    dlog("%u: SYMENT is not %lu", __LINE__, sizeof(Elf64_Sym));
+                    return false;
+                }
+                break;
+            case DT_RELA: GET_DT(rela);
+            case DT_RELASZ: GET_DT(relasz);
+            case DT_RELAENT:
+                if (e->d_un.d_val != sizeof(Elf64_Rela)) {
+                    dlog("%u: RELAENT is not %lu", __LINE__, sizeof(Elf64_Rela));
+                    return false;
+                }
+                break;
+
+            //---- plt, got related ----
+            case DT_PLTGOT: GET_DT(pltgot);
+            case DT_PLTRELSZ: GET_DT(pltrelsz);
+            case DT_PLTREL: /* expected DT_RELA */ GET_DT(pltrel);
+            case DT_JMPREL: GET_DT(jmprel);
+
+            case DT_TEXTREL: break;
+            case DT_RELACOUNT: GET_DT(relacount);
+            default:
+                dlog("%u: not supported d_tag: %ld", __LINE__, e->d_tag);
+                return false;
+        }
+    }
+
+    return true;
+//    /* update values of special symbols in .dynsym */
+//    update_dynsym(dinfo.symtab, (char *)(encl_base + dinfo.strtab),
+//            spec_dsym, nspec_dsym);
+}
+
+void relocate(Elf64_Sym *symtab, Elf64_Rela *reltab, unsigned nrel, uint64_t vaddr_base)
+{
+    unsigned i;
+    uint64_t ofs;
+    uint32_t sym;
+    uint32_t type;
+
+    for (i = 0;i < nrel;++i) {
+        ofs = vaddr_base + reltab[i].r_offset;
+        sym = ELF64_R_SYM(reltab[i].r_info);
+        type = ELF64_R_TYPE(reltab[i].r_info);
+
+        if (type == R_X86_64_64) { /* word 64 */
+            *(uint64_t *)ofs = symtab[sym].st_value + reltab[i].r_addend;
+            dlog_i("%lx\n", *(uint64_t *)ofs);
+        } else if (type == R_X86_64_32) { /* word 32 */
+            *(uint32_t*)ofs = (uint32_t)(symtab[sym].st_value + reltab[i].r_addend);
+            dlog_i("%x\n", *(uint32_t *)ofs);
+        } else if (type == R_X86_64_32S) { /* word 32 */
+            *(int32_t*)ofs = (int32_t)(symtab[sym].st_value + reltab[i].r_addend);
+            dlog_i("%x\n", *(int32_t *)ofs);
+        } else if (type == R_X86_64_PC32 || type == R_X86_64_PLT32) { /* word 32 */
+            *(uint32_t*)ofs = (uint32_t)(symtab[sym].st_value - ofs
+                    + reltab[i].r_addend);
+            dlog_i("%x\n", *(uint32_t *)ofs);
+        } else if (type == R_X86_64_GOTPCREL) { /* word 32 */
+            *(uint32_t*)ofs = (uint32_t)((Elf64_Addr)&(symtab[sym].st_value)
+                    - ofs + reltab[i].r_addend);
+            dlog_i("%x\n", *(uint32_t *)ofs);
+        } else if (type == R_X86_64_RELATIVE) { /* word 64 */
+            *(uint64_t *)ofs += vaddr_base;
+            dlog_i("%lx\n", *(uint64_t *)ofs);
+        } else if (type == R_X86_64_JMP_SLOT || type == R_X86_64_GLOB_DAT) {
+            /* word 64 */
+            *(uint64_t *)ofs = symtab[sym].st_value;
+            dlog_i("JMP_SLOT %lx\n", *(uint64_t *)ofs);
+        } else
+            dlog("%u: Relocation -- not supported type %u", __LINE__, type);
+    }
 }
 
 void *tlopen(const char *name,  tltype_t type) {
     tlhandle_t *handle;
+    uint64_t dynamic;
+    Elf64_Dyn *dyn;
+    Elf64_Sym *sym;
+
     if (type != TL_ELF_TYPE_EXEC && type != TL_ELF_TYPE_SHARED)
         return NULL;
 
+    /* mmap ELF file */
     handle = (tlhandle_t *) malloc(sizeof(tlhandle_t));
     handle->fd = open(name, O_RDONLY);
     handle->fsz = file_size(handle->fd);
@@ -158,9 +300,31 @@ void *tlopen(const char *name,  tltype_t type) {
             PROT_READ | PROT_WRITE,
             MAP_PRIVATE, handle->fd, 0);
 
+    /* check ELF header */
     handle->ehdr = (Elf64_Ehdr *) handle->memmap;
     validate_ehdr(handle->ehdr, type);
 
+    /* load based program header */
     handle->phdr = (Elf64_Phdr *) ((uint64_t) handle->memmap + (uint64_t) handle->ehdr->e_phoff);
-    load(handle->ehdr, handle->phdr);
+    handle->vaddr_base = load(handle->ehdr, handle->phdr);
+    if (handle->vaddr_base == NULL) {
+        dlog("%u: nothing to load", __LINE__);
+        return NULL;
+    }
+
+    /* read dynamic section */
+    dynamic = find_dynamic(handle->ehdr, handle->phdr);
+    dyn = (Elf64_Dyn *)((uint64_t) handle->vaddr_base + dynamic);
+    read_dynamic(dyn, &handle->dyn);
+
+    /* update symbol table --> fill undefined symbol locations */
+    handle->shdr = (Elf64_Shdr *) ((uint64_t) handle->memmap + (uint64_t) handle->ehdr->e_shoff);
+    sym = (Elf64_Sym *)((uint64_t) handle->memmap + handle->dyn.symtab);
+    update_dynsym(handle->shdr, handle->ehdr->e_shnum, sym, handle->dyn.symtab);
+
+    /* relocate */
+    relocate((Elf64_Sym *)((uint64_t) handle->vaddr_base + handle->dyn.symtab),
+            (Elf64_Rela *)((uint64_t) handle->vaddr_base + handle->dyn.jmprel),
+            handle->dyn.pltrelsz / sizeof(Elf64_Rela),
+            (uint64_t) handle->vaddr_base);
 }
--- tinylinker/tinylinker_internal.h
+++ tinylinker/tinylinker_internal.h
@@ -1,3 +1,21 @@
+/* data used in dynamic linking */
+struct dynlink_info {
+    uint64_t hash; // TODO: currently, .hash is not used .. improve performance! */
+    uint64_t strtab;
+    uint64_t symtab;
+    size_t symtabsz;
+    uint64_t strsz;
+    uint64_t pltgot;
+    uint64_t pltrelsz;
+    uint64_t pltrel;
+    uint64_t jmprel;
+    uint64_t rela;
+    uint64_t relasz;
+    uint64_t relacount; /* I think this is meaningless .. */
+};
+
+typedef struct dynlink_info dyninfo_t;
+
 typedef struct tl_handle {
     // for file op
     int fd;
@@ -7,7 +25,9 @@ typedef struct tl_handle {
     // ELF
     Elf64_Ehdr *ehdr;
     Elf64_Phdr *phdr;
+    Elf64_Shdr *shdr;
 
     // load
-    void *program;
+    void *vaddr_base;
+    dyninfo_t dyn;
 } tlhandle_t;
